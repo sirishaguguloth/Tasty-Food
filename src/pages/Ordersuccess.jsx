09c1e1053b7eac7ce89b1b1d6bfa246de28ce37c
sirishaguguloth <sirishagugulothu777@gmail.com>
2024-07-12 22:30:58 +0530

    style-tasyfood

--- src/pages/Ordersuccess.jsx
+++ src/pages/Ordersuccess.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import '../styles/orders.css'
 
 export default function Ordersuccess() {
   return (
     <div className='ordersuc'> 
      <h2>Your Order is Successfull. Go to Orders</h2>
-     <button ><Link className='link' to={'/order'}>Orders</Link></button>
+     <button  className='ordersucbtn'><Link className='link' to={'/order'}>Orders</Link></button>
     </div>
   )
 }
