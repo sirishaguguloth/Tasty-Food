ea1064b0c25270dd74f2cb1c60d18d40e90c0f95
sirishaguguloth <sirishagugulothu777@gmail.com>
2024-07-19 14:40:06 +0530

    res navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
-import '../styles/navbar.css';
-import { HiMenu } from "react-icons/hi";
+import React, { useState } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { HiMenu, HiX } from "react-icons/hi";
 import { Link, useNavigate } from 'react-router-dom';
+import '../styles/navbar.css';
 
-export default function Navbar() {
+const Navigation = () => {
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   const logout = () => {
     localStorage.removeItem("userlogin");
     return navigate('/login');
-  }
+  };
+
   return (
-    <div className='navbar'>
-      <div className="navwrap">
-      <div className="part1"><h2 className="logo">Logo</h2></div>
-      <div className="icon"><HiMenu /></div>
-      <div className='part2'>
-        <ul className='navlists'>
-          <li className='listitems'><Link to={'/'} className='links'>Home</Link></li>
-          <li className='listitems'><Link to={'/menu'} className='links'>Menu</Link></li>
-          <li className='listitems'><Link to={'/contact'} className='links'>Contact</Link></li>
-          <li className='listitems'><Link to={'/cart'} className='links'>Cart</Link></li>
-          <li className='listitems'><Link to={'/order'} className='links'>Order</Link></li>
-          <li className='listitems'><button className='navaction' onClick={logout}>Logout</button></li>
-        </ul>
-      </div>
-      </div>
-    </div>
-  )
-}
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand className='logo'>LOGO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" onClick={toggleMenu}>
+          {menuOpen ? <HiX /> : <HiMenu />}
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbarScroll" className={`${menuOpen ? 'show' : ''}`}>
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+          </Nav>
+          <ul className='navlists'>
+            <li className='listitems'><Link to={'/'} className='links'>Home</Link></li>
+            <li className='listitems'><Link to={'/menu'} className='links'>Menu</Link></li>
+            <li className='listitems'><Link to={'/contact'} className='links'>Contact</Link></li>
+            <li className='listitems'><Link to={'/cart'} className='links'>Cart</Link></li>
+            <li className='listitems'><Link to={'/order'} className='links'>Order</Link></li>
+            <li className='listitems'><button className='navaction' onClick={logout}>Logout</button></li>
+          </ul>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
